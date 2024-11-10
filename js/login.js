dd5e93527f324ae2fa3c8a80e9d091d032ce5da0
juan lescano <juanlescano93@hotmail.com>
2024-11-10 15:06:11 -0300

    Creando login

---
+++ js/login.js
@@ -0,0 +1,9 @@
+function loguear(){
+
+    let email = document.getElementById('email').value;
+    let btnContinuar = document.getElementById('continuar-btn').value;
+    let crearCuenta = document.getElementById('login-btn').value;
+    let iniciarGoogle = document.getElementById('iniciarSesion-btn').value;
+    
+    
+    }
